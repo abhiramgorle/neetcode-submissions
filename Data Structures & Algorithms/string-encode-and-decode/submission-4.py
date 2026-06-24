80942b2ab259ba95c3ed7735a0dce5aee3107f7c
Abhiram Gorle <80030901+abhiramgorle@users.noreply.github.com>
2026-06-24 15:37:11 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs == []: return " "
+        return "©".join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        if s == "" : return [""]
+        elif s == " ": return []
+        return s.split("©")
